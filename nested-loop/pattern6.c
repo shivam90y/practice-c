f48b5d20dea362c6432a9ff76113db7e361a632b
Shivam Yadav <shivamy@coindelta.com>
2018-12-14 15:46:45 +0530

    pattern6

--- nested-loop/pattern6.c
+++ nested-loop/pattern6.c
@@ -2,17 +2,17 @@
 int main()
 {
     int i, j;
-    for (i = 1; i <= 5; i++)
+    for (i = 5; i >= 1; i--)
     {
         for (j = 1; j <= i; j++)
             printf("%d", j);
-            printf("\n");
+        printf("\n");
     }
-    for (i = 4; i >= 1; i--)
+    for (i = 2; i <= 5; i++)
     {
         for (j = 1; j <= i; j++)
             printf("%d", j);
-            printf("\n");
+        printf("\n");
     }
     return 0;
 }
