3f95ff71511c71fe0bef70f87135b8997a8ecd31
Shivam Yadav <shivamy@coindelta.com>
2018-12-14 22:27:16 +0530

    pattern9.c

---
+++ nested-loop/pattern9.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void shiv(int num)
+{
+    int i, j, k = 1;
+    for (i = 1; i <= num; i++)
+    {
+        for (j = num; j >= i; j--)
+        {
+            printf(" ");
+        }
+        for (k = 1; k <= i; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        k = k + 1;
+    }
+}
+int main()
+{
+    int input;
+    scanf("%d", &input);
+    shiv(input);
+    return 0;
+}
