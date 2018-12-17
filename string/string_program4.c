82a5da2c581eb2db7de9909aa5ddc9379760f347
Shivam Yadav <shivamy@coindelta.com>
2018-12-17 16:15:08 +0530

    string

--- string/string_program4.c
+++ string/string_program4.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
-int main()
+void shivam(int num)
 {
     int i, j;
-    char arr[12] = {'s', 'h', 'i', 'v', 'a', 'm', ' ', 'y', 'a', 'd', 'a', 'v'};
-    for (i = 0; i < 12; i++)
+    char arr[100] = {'S', 'H', 'I', 'V', 'A', 'M', ' ', 'Y', 'A', 'D', 'A', 'V', ' ', 's', 'h', 'i', 'v', 'a', 'm', ' ', 'y', 'a', 'd', 'a', 'v'};
+    for (i = 0; i < num; i++)
     {
         for (j = 0; j < 1; j++)
             printf("%c", arr[i]);
     }
+    printf("\n");
+}
+int main()
+{
+    int input;
+    scanf("%d", &input);
+    shivam(input);
     return 0;
 }
---
+++ string/string_reverse.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    char arr[14] = {'S','H','I','V','A','M','Y','A','D','A','V'};
+    for (i = 11; i >= 0; i--)
+        printf("%c", arr[i]);
+        printf("\n");
+    return 0;
+}
